95e3907341da916cb410615e41526bb9f1c89dfc
simo <1394260116@qq.com>
2022-04-14 19:57:15 +0800

    1.短评长评论都可以存了 2.一个问题，追加数据表头会重复添加

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import json
 from lxml import etree
-import re
+import os
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -14,13 +14,19 @@ headers = {
 }
 
 cookies = {
-    'Cookies':'bid=qjf1zprHQ5A; __utmc=30149280; __utmc=223695111; ll="118371"; viewed="2252862"; douban-fav-remind=1; __utmz=30149280.1648520945.3.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __gads=ID=ef8521945c328054-221cccdb67d100c9:T=1648520947:RT=1648520947:S=ALNI_MYHsgxNZ-ge5AWgppYWYvx1JN_EjQ; push_noty_num=0; push_doumail_num=0; __utmv=30149280.25306; _vwo_uuid_v2=DB4BCA4273F64DF968E081C011B9A3B40|09689baca0f48fb5c4421933f6fd8a2e; __utmz=223695111.1649859827.5.5.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; ap_v=0,6.0; _pk_ref.100001.4cf6=["","",1649900533,"https://www.douban.com/"]; _pk_ses.100001.4cf6=*; __utma=30149280.1250153523.1648352387.1649859820.1649900533.7; __utmb=30149280.0.10.1649900533; __utma=223695111.1357127237.1648352387.1649859827.1649900533.6; __utmb=223695111.0.10.1649900533; dbcl2="253066269:LGDN2yzpYaA"; ck=J19w; _pk_id.100001.4cf6=794296baade17d9f.1648352387.6.1649904925.1649860142.'
+    'Cookies':'bid=qjf1zprHQ5A; __utmc=30149280; ll="118371"; viewed="2252862"; douban-fav-remind=1; __utmz=30149280.1648520945.3.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __gads=ID=ef8521945c328054-221cccdb67d100c9:T=1648520947:RT=1648520947:S=ALNI_MYHsgxNZ-ge5AWgppYWYvx1JN_EjQ; push_noty_num=0; push_doumail_num=0; __utmv=30149280.25306; dbcl2="253066269:LGDN2yzpYaA"; ck=J19w; ap_v=0,6.0; _pk_ses.100001.8cb4=*; __utma=30149280.1250153523.1648352387.1649900533.1649933344.8; __utmt=1; _pk_id.100001.8cb4=d924c444428e1795.1648353221.5.1649933347.1649859825.; __utmb=30149280.4.10.1649933344'
 }
 
-def duanping(id = 0 ,file = 0,num = 0,hh = 1):
+
+def save_csv(total_data,path):
+    mydata = pd.DataFrame(total_data, columns=['name', 'star', 'vote_count', 'time', 'text'])
+    mydata.to_csv(path, mode='a+', index=False, encoding="utf_8_sig")
+
+
+def duanping(id, save_dir):
     url = "https://movie.douban.com/subject/"+str(id)+"/comments"
 
-    for page_start in range(0, 20, 20):
+    for page_start in range(0, 500, 20):
         if page_start == 0:
             params = {
                 "sort": "new_score",
@@ -37,31 +43,30 @@ def duanping(id = 0 ,file = 0,num = 0,hh = 1):
         response = requests.get(
             url=url,
             headers=headers,
-            params=params
+            params=params,
+            cookies=cookies
         )
+
         total_data = []
-        tree = etree.HTML(response.text)
-        div_list = tree.xpath('//*[@id="comments"]/div')
+
+        bs = BeautifulSoup(response.text,'lxml')
+        div_list = bs.find_all('div',attrs={'class':'comment'})
         print(len(div_list))
-        for list in div_list[:20]:
-            vote_count = list.xpath('.//span[@class="votes vote-count"]/text()')[0]
-            name = list.xpath('.//span[@class="comment-info"]/a/text()')[0]
+        for div in div_list:
+            vote_count = div.find('span',attrs={'class':'votes vote-count'}).text
+            name = div.find('span',attrs={'class':'comment-info'}).find('a').text
             try:
-                star_level = re.search('\d', list.xpath('.//span[@class="comment-info"]/span[2]/@class')[0]).group()
+                star_level = div.find('span',attrs={'class':'comment-info'}).find_all('span')[1].get('title')
             except:
-                star_level = ' '
-            comment = str(list.xpath('.//span[@class="short"]/text()')[0])
-            if(bool(hh)):
-                comment = comment.replace('\n','').replace('\r','')
-            pb_time=list.xpath('.//span[@class="comment-info"]/span[@class="comment-time "]/text()')[0].strip('\n').strip(' ').strip('\n')
+                star_level = ''
+
+            pb_time = div.find('span',attrs={'class':'comment-info'}).find_all('span')[-1].get('title')
+            comment = div.find('span', attrs={'class':'short'}).text
             data=[name,star_level,vote_count,pb_time,comment]
             total_data.append(data)
-        mydata=pd.DataFrame(total_data,columns=['name','star','vote_count','time','text'])
-        if page_start==0:
-            mydata.to_csv('./'+str(id)+'.csv',index=False,encoding="utf_8_sig")
-        else:
-            mydata.to_csv('./'+str(id)+'.csv',index=False,encoding="utf_8_sig",mode='a',header=False)
-        time.sleep(0.08)
+        save_csv(total_data, path=save_dir+'/short_comments.csv')
+        time.sleep(0.1)
+
 
 def yingping(id):
     """
@@ -69,8 +74,14 @@ def yingping(id):
     :param id:
     :return:
     """
+    ### 短评论
+    save_path = f'./{id}/comment'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    duanping(id, save_path)
     url = "https://movie.douban.com/subject/"+str(id)+"/reviews"
-    for page_start in range(0, 40, 20):
+    for page_start in range(0, 500, 20):
 
         print(f"page_start {page_start}")
         params = {
@@ -87,22 +98,25 @@ def yingping(id):
         bs = BeautifulSoup(response.text, 'lxml')
         comment_div_list = bs.find_all('div', attrs={'class':'main review-item'})
 
-        # # 如果当前页已经没有影评退出循环
-        # if len(comment_div_list)==0:
-        #     break
-        # print(len(comment_div_list))
-        for comment_div in comment_div_list[:5]:
+        #如果当前页已经没有影评退出循环
+        if len(comment_div_list)==0:
+            break
 
-            id = comment_div.get('id')
+        for comment_div in comment_div_list:
+
+            comment_id = comment_div.get('id')
             # print(id)
-            # print(type(vote_count))
-            print(id)
+
+            print(comment_id)
+            save_path = f'./{id}/comment'
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
             # 用changpinglun加载一下长评论
-            changpinglun(id)
+            changpinglun(comment_id,save_path)
 
 
 
-def changpinglun(id = 0):
+def changpinglun(id,save_dir):
     """
     根据评论id 获取长评论
     :param id:
@@ -117,16 +131,33 @@ def changpinglun(id = 0):
 
     content = response.text
     bs = BeautifulSoup(content,'lxml')
-    # print(bs.find('div',id='link-report'))
+    # print(content)
+    vote_count = bs.find('div', attrs={'class': 'main-panel-useful'}).find('button').text.split(' ')[-4]
+    # print(vote_count)
+    name = bs.find('header', attrs={'class': 'main-hd'}).find_all('span')[0].text
+
+    try:
+        star_level = bs.find('span', attrs={'class': 'main-title-rating'}).get('title')
+    except:
+        star_level = ''
+
+    pb_time = bs.find('header', attrs={'class': 'main-hd'}).find_all('span')[-1].text
+
     comment_list = bs.find('div', attrs={'class':'main-bd'}).find_all('p')
     comments = ''
+
+
     for comment_p in comment_list:
         comments = comments + comment_p.text +'\n'
 
-
-    print(comments)
+    # print(comments)
+    comments=comments.strip('"')
+    data = [name, star_level, vote_count, pb_time, comments]
+    save_csv([data],path=save_dir+'/long_comments.csv')
     time.sleep(2)
     return comments
 
 
-duanping(26825482)
+# changpinglun(26825482)
+
+yingping(26825482)
